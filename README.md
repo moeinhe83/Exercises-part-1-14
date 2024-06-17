379d87c9045be30659217af468e701f7c893c2f4
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-06-17 19:07:26 +0330

    Upload Exercises-part-1-14 Project On Github

---
+++ README.md
@@ -0,0 +1,27 @@
+Project Name ===> Exercises Part 1
+
+Question Number ===> 14
+
+Programming Language ===> Python
+
+Program description ===> 14th Exercise
+
+Example ===> Write a program that displays the following tuple in order and then displays the reverse:
+
+A => (3, 4, 5, 2, 1, 7, 8, 9, 6, 10)
+
+Description Of Files:
+
+practice_14_linux.py ===> For Linux [Ubuntu Or Kali]
+
+For Run ===> python practice_14_linux.py
+
+practice_14_windows.py ===> For Windows
+
+For Run ===> python practice_14_windows.py
+
+Required packages For Python ===> os / termcolor2 / pyfiglet
+
+termcolor2 ===> To install => pip install termcolor2
+
+pyfiglet ===> To install => pip install pyfiglet
---
+++ practice_14_linux.py
@@ -0,0 +1,31 @@
+# Practice_14
+
+# Library
+from os import system as sys
+from termcolor2 import colored
+from pyfiglet import figlet_format
+
+# Clear Terminal
+sys('clear')
+
+# Intro
+print(colored(figlet_format("Exercises Part 1"), color='cyan'))
+print(colored('=====================================================================', color='red'))
+print(colored('Question Number ===> 14', color='blue'))
+print(colored('// Be Sure To Read The README File To See The Question //', color='blue'))
+print('                                            ')
+
+# Value 
+number = (3, 4, 5, 2, 1, 7, 8, 9, 6, 10)
+
+# Convert Tuple To List
+number = list(number)
+
+# Display In Reverse Order
+number.sort()
+print(number)
+number.sort(reverse=True)
+print(number)
+
+# Finish
+# Create By Moein Heshmati
